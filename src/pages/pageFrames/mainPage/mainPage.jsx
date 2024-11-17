516c6305874eb883eb52a52906a42dcad563b636
Jihee Byun <jh910805jh@gmail.com>
2024-11-17 21:08:44 +0900

    outline

---
+++ src/pages/pageFrames/mainPage/mainPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+
+
+
+export const MainPage = () => {
+
+
+    return (
+        <div>
+            <div>
+                {/*  navbar place */}
+                <div> </div>
+                {/* section place */}
+                <section> </section>
+                {/* bottom section place */}
+                <section></section>
+            </div>
+
+        </div>
+    ) 
+}
+
+
+// 1200이하 
