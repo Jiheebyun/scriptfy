fe9fe51aadbb26b9c25be501b6b8b79b7b98289e
Jihee Byun <jh910805jh@gmail.com>
2024-11-22 23:01:34 +0900

    styles

--- src/pages/components/topNavBar/topNavBar.jsx
+++ src/pages/components/topNavBar/topNavBar.jsx
@@ -9,10 +9,14 @@ export const TopNavBar = () => {
     return (
         <div className="top-nav-bar">
             <div className="layout-topbar-news">
+                <div className="news">
 
+                </div>
             </div>
             <div className="layout-top-bar">
+                <div className="top-menu">
 
+                </div>
             </div>
         </div>
     )
