2bb9b123c976e9e07875ea9f686a2e5f8af62d35
Jihee Byun <jh910805jh@gmail.com>
2024-11-28 22:01:52 +0900

    styles

--- src/components/topNavBar/topNavBar.tsx
+++ src/components/topNavBar/topNavBar.tsx
@@ -3,15 +3,17 @@ import React from "react";
 import './topNavBar.scss'
 
 
+
 export const TopNavBar = () => {
+    const imagePath = process.env.REACT_APP_IMAGE_PATH;
 
-
+    console.log(imagePath)
     return (
         <div className="top-nav-bar">
             <div className="layout-top-bar">
                 <div className="top-menu">
                     <div className="logo-container">
-
+                        <img src={`${imagePath}logo.png`} alt="logo"></img>
                     </div>
                     <div className="icons-container">
 
