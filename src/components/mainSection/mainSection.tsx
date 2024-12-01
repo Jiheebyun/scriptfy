f0528434c9695800a85c97f6ce0e190f6656beb6
Jihee Byun <jh910805jh@gmail.com>
2024-12-01 21:18:41 +0900

    working on the content

--- src/components/mainSection/mainSection.tsx
+++ src/components/mainSection/mainSection.tsx
@@ -8,10 +8,13 @@ export const MainSection = () => {
 
         <section className="section-layout">
             <div className="section-container">
-                <div>
-
+                <div className="section-content-container">
+                    <h1 className="content-title">The Most Complete UI Suite for <span>React.js</span></h1>
+                    <p>
+                        Elevate your web applications with PrimeReact's comprehensive suite of customizable, feature-rich UI components. With PrimeReact, turning your development vision into reality has never been easier.  
+                    </p>
                 </div>
-                <div>
+                <div className="section-example-container">
 
                 </div>
             </div>
