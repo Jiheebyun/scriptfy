55916579c600bff3d7bff910b87e653b8726f431
Jihee Byun <jh910805jh@gmail.com>
2024-12-05 22:58:38 +0900

    modified rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
 import { terser } from 'rollup-plugin-terser';
 
-export default [
-  {
-    input: 'src/library/index.ts',
-    output: [
-      {
-        file: 'dist/react/my-ui-library.react.esm.js',
-        format: 'esm',
-        sourcemap: true
-      },
-      {
-        file: 'dist/react/my-ui-library.react.cjs.js',
-        format: 'cjs',
-        sourcemap: true
-      }
-    ],
-    plugins: [resolve(), commonjs(), typescript(), terser()],
-    external: ['react', 'react-dom']
-  },
-  {
-    input: 'src/library/index.js',
-    output: [
-      {
-        file: 'dist/vanilla/my-ui-library.vanilla.umd.js',
-        format: 'umd',
-        name: 'MyUILibrary',
-        sourcemap: true
-      }
-    ],
-    plugins: [resolve(), commonjs(), terser()]
-  }
-];
+export default {
+  input: 'src/library/index.ts',
+  output: [
+    {
+      file: 'dist/react/scriptify.react.esm.js',
+      format: 'esm',
+      sourcemap: true
+    },
+    {
+      file: 'dist/react/scriptify.react.cjs.js',
+      format: 'cjs',
+      sourcemap: true
+    }
+  ],
+  external: ['react', 'react-dom'], 
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript(),
+    postcss({ extract: true }),
+    terser()
+  ]
+};
