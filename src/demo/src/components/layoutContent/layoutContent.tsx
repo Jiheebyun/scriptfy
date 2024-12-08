fc772a84928dc82f8d5366decbb82ca199afc401
Jihee Byun <jh910805jh@gmail.com>
2024-12-08 22:05:24 +0900

    added layout for contents

---
+++ src/demo/src/components/layoutContent/layoutContent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './layoutContent.scss'
+
+
+export const layoutContent = () => {
+    return (
+        <div className="mainFrame">
+            <div className="main-container">
+
+
+            </div>
+        </div>
+    )
+}
