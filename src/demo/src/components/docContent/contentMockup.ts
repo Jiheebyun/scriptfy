f1764f04b1856397593f6e97b5539ed6e821c1fd
Jihee Byun <jh910805jh@gmail.com>
2025-01-05 18:15:33 +0900

    Content Data Structure Design

--- src/demo/src/components/docContent/contentMockup.ts
+++ src/demo/src/components/docContent/contentMockup.ts
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-
-
-const data = {
+const data ={
     title: "",
     content: "",
     id: "",
@@ -43,25 +40,3 @@ const data = {
         },
     ]
 }
-
-
-const DocContent = () => {
-    const mockupData = useState()
-    // 해당 컴포넌트의 기능 
-    // 해당 하는 내용의  title UI 
-    // 컴포넌트의 import  설명 UI
-    // Basic 기능 설명 UI
-
-    // 같은 기능의 부수적인 기능이 결합된 기능 설명 UI
-
-    
-
-    return (
-        <div className="temp">
-            <h1>Title: Basic Setting</h1>
-            <p>awefawef</p>
-        </div>
-    )
-}
-
-export default DocContent
