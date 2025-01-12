75f37dc8cf4acc82642aa86c4e0605ea6285af0a
Jihee Byun <jh910805jh@gmail.com>
2025-01-12 20:42:56 +0900

    working on the content page

--- src/demo/src/components/docContentIntro/docContentIntro.tsx
+++ src/demo/src/components/docContentIntro/docContentIntro.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 import './docContentIntro.scss'
@@ -6,13 +6,15 @@ import './docContentIntro.scss'
 
 const DocContentIntro = ({data}) => {
     console.log(`Intro`)
-    console.log(data)
-    data.title
+    console.log(data.title)
+    // useEffect(() => { // data가 존재하는지 확인하고, 존재하지 않으면  ErrorPage
+
+    // }, []),
     return(
         <>
             <div className="doc-intro">
-                <h1>{data.title}</h1>
-                <p>{data.content}</p>
+                <h1>{data && data.title}</h1>
+                <p>{data && data.content}</p>
             </div>
         </>
     )
