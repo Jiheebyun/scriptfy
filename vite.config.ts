332fb4c9a0d28076cbb543425a72bf0cb4e3e5bc
Jihee Byun <jh910805jh@gmail.com>
2025-01-30 11:44:45 +0900

    added a client folder for the front-end

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src/demo/'),
+  root: path.resolve(__dirname, 'src/demo/client'),
   publicDir: path.resolve(__dirname, 'src/demo/public'),
   plugins: [react()],
   server: {
@@ -12,12 +12,12 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src/demo/src'),
+      '@': path.resolve(__dirname, 'src/demo/client'),
       'library': path.resolve(__dirname, 'src/library')
     },
   },
   build: {
-    outDir: path.resolve(__dirname, 'dist/demo'),
+    outDir: path.resolve(__dirname, 'dist/demo/client'),
     emptyOutDir: true
   },
 });
