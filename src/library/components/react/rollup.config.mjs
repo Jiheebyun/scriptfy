16515f35e26239cd7308cb62dbc976a7773b47da
Jihee Byun <jh910805jh@gmail.com>
2025-02-05 22:34:25 +0900

    change build structure to import components

--- src/library/components/react/rollup.config.mjs
+++ src/library/components/react/rollup.config.mjs
@@ -3,30 +3,53 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import postcss from 'rollup-plugin-postcss';
 
-export default {
-  input: 'src/index.ts',   // 라이브러리 진입점
-  output: [
-    {
-      file: 'dist/my-library.esm.js',
+export default [
+  // ESM
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist/esm',
       format: 'esm',
       sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].js'
     },
-    {
-      file: 'dist/my-library.cjs.js',
+    external: ['react', 'react-dom'],
+    plugins: [
+      resolve(),
+      typescript({
+        tsconfig: './tsconfig.json'  // 여기에는 "declaration": false
+      }),
+      postcss({
+        extensions: ['.scss'],
+        extract: true,
+        minimize: true
+      })
+    ]
+  },
+  // CJS
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist/cjs',
       format: 'cjs',
       sourcemap: true,
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].cjs'
     },
-  ],
-  external: ['react', 'react-dom'], // 번들에서 제외
-  plugins: [
-    resolve(),
-    typescript({
-      tsconfig: './tsconfig.json' // 이 패키지 내의 tsconfig.json
-    }),
-    postcss({
-      extensions: ['.scss'],
-      extract: true,   // 별도 CSS 파일 생성 가능
-      minimize: true
-    })
-  ]
-};
+    external: ['react', 'react-dom'],
+    plugins: [
+      resolve(),
+      typescript({
+        tsconfig: './tsconfig.json'
+      }),
+      postcss({
+        extensions: ['.scss'],
+        extract: true,
+        minimize: true
+      })
+    ]
+  }
+];
