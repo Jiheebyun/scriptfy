eca0b6d7a2df09042267af89674031d4c1651e28
Jihee Byun <jh910805jh@gmail.com>
2025-02-08 22:13:15 +0900

    added README file

--- src/demo/client/src/pages/mainPage/mainPage.tsx
+++ src/demo/client/src/pages/mainPage/mainPage.tsx
@@ -6,7 +6,8 @@ import { TopNewsBar } from "../../components/topNewsBar/topNewsBar";
 import  LayoutContent  from "../../components/layoutContent/layoutContent";
 import Footer from "../../components/footer/footer";
 
-import  Button  from '@scriptify/react/button'
+import  Button  from '@scriptify/react/button' // 이런 형식으로  import 해야한다.
+// import { Button } from '@scriptify/react/button'
 
 // import { BottomSection } from "../../../components/bottomSection/bottomSection";
 
