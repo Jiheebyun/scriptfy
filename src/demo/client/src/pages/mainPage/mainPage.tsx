492e697b6a0ec35a685a39d13e550085aea6d212
Jihee Byun <jh910805jh@gmail.com>
2025-02-09 18:46:24 +0900

    The scriptify_js library is published to npm and imported in the demo project

--- src/demo/client/src/pages/mainPage/mainPage.tsx
+++ src/demo/client/src/pages/mainPage/mainPage.tsx
@@ -6,9 +6,7 @@ import { TopNewsBar } from "../../components/topNewsBar/topNewsBar";
 import  LayoutContent  from "../../components/layoutContent/layoutContent";
 import Footer from "../../components/footer/footer";
 
-import  Button  from '@scriptify/react/button' // 이런 형식으로  import 해야한다.
-// import { Button } from '@scriptify/react/button'
-
+import { Button } from "@scriptify_js/react";
 // import { BottomSection } from "../../../components/bottomSection/bottomSection";
 
 const MainPage = () => {
