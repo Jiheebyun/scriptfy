df20ec79cc8f12635095e2965c960d60cc2d4a89
Jihee Byun <jh910805jh@gmail.com>
2025-02-15 22:58:00 +0900

    added comments for the react component

--- src/library/components/react/src/components/button/Button.tsx
+++ src/library/components/react/src/components/button/Button.tsx
@@ -49,13 +49,19 @@ export default Button;
 Parameters
     label:(String) - 버튼에 원하는 텍스트를 넣을수 있다. 
         ex: <Button label="Submit" />
+
     className:(String) - 클래스 속성을 지정 할 수 있다
         ex: <Button className="isActive" />
+
     onClick:(Function) - 클릭이벤트의 콜백 함수가 실핼된다.
         ex: <Button onClick={btnHandler} />
+
     href:(String) - 해당 링크가 새창에서 띄워진다.
         ex: <Button href="www.google.com" />
+
+    color:(String) - 컬러 속성은 버튼의 컬러만 바로 바꿀수 있다.
+        ex: <Button color="blue" />
+        
     style:(object) - 버튼의 상세 스타일을 지정할수 있다. color와 style에 background color를 두개의 속성이 지정되면 style의 스타일을 우선으로 적용한다
         ex: <Button style={{backgroundColor:gray}} color: "blue"> **style 우선 적용**
-
 */
