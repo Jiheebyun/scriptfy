42cf7e4b520f9e7ab3540d473ac35609e8260d39
Jihee Byun <jh910805jh@gmail.com>
2025-03-12 22:27:47 +0900

    working on the searching feature for the input component

--- src/library/components/react/src/components/input/Input.tsx
+++ src/library/components/react/src/components/input/Input.tsx
@@ -4,12 +4,16 @@ import "./input.scss";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
   type?: string;
-  iconClassName?: string; 
+  iconClassName?: string;
+  search?: boolean;
+  searchData?: any[];
 }
 
 const SfInput: React.FC<InputProps> = ({
   type = "text",
   placeholder,
+  search,
+  searchData,
   iconClassName = 'bx bxs-user', // 아이콘을 제공할수 있게 만드면 어떨까
   ...rest // 나머지 input 속성들 (value, onChange, disabled 등)
 }) => {
