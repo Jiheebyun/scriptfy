addb566008426de1267d76437ddc8ddb09eaa178
Jihee Byun <jh910805jh@gmail.com>
2025-03-13 23:05:58 +0900

    added a comment on the input component

--- src/library/components/react/src/components/input/Input.tsx
+++ src/library/components/react/src/components/input/Input.tsx
@@ -17,6 +17,11 @@ const SfInput: React.FC<InputProps> = ({
   iconClassName = 'bx bxs-user', // 아이콘을 제공할수 있게 만드면 어떨까
   ...rest // 나머지 input 속성들 (value, onChange, disabled 등)
 }) => {
+
+    // Search가 true일때, searchData를 읽어서, 
+    // 사용자가 타이핑을 하면 데이터에 해당하는 단어를 하위에 보여주는 기능 추가
+
+
   return (
     <div className="input-group">
       <i className={iconClassName}></i>
