9dd340856216944a109d90d98f9f8358f7a8a775
Jihee Byun <jh910805jh@gmail.com>
2025-03-13 23:37:30 +0900

    off

--- src/library/components/react/src/components/input/Input.tsx
+++ src/library/components/react/src/components/input/Input.tsx
@@ -22,6 +22,8 @@ const SfInput: React.FC<InputProps> = ({
     // 사용자가 타이핑을 하면 데이터에 해당하는 단어를 하위에 보여주는 기능 추가
 
 
+    
+
   return (
     <div className="input-group">
       <i className={iconClassName}></i>
