0fa1f06e1ae68ad8c45759dbf1f40a9521f4b65e
Jihee Byun <jh910805jh@gmail.com>
2025-05-08 13:04:34 +0900

    working on the drag and drop feature

---
+++ src/library/components/react/src/components/dragDrop/DragDrop.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+
+// Sortable List : todo list같은 순서 바뀌는 순서가 바뀌는
+// Kanban style : 여러 컬럼(그룹) 간의 드래그 이동
+// Free Positioning : 캔버스 아무 곳에나 배치 x y 좌표 기반(노드를 원하는 위치에 배치)
+// File upload : 드래드로 파일을 업로드 할수 있는 기능 
+
+
+
+const SfDragDrop = () => {
+
+
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+
