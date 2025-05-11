669fe11007b8e7c3d6f0f9c2215dc2a51b5d6ff7
Jihee Byun <jh910805jh@gmail.com>
2025-05-11 22:48:38 +0900

    working on the kanban drag and drop feature

--- src/library/components/react/src/components/dragDrop/subComponents/KanbanBoard.tsx
+++ src/library/components/react/src/components/dragDrop/subComponents/KanbanBoard.tsx
@@ -6,7 +6,23 @@ export interface KanbanProps {
     onChange?: (next: string[]) => void;
 }
 
+
+// Mockup Props
+// kanbanWidth
+// kanbanHeight
+// kanbanBoard: [ 
+//     {title: In Progress, key: progress, color: red}, 
+//     {title: On Hold, key: hold , color: blue}, 
+//     {title: waiting for Details, key: details, color: purple} 
+// ]
+// kanbanCard: [{} ,{} ,{}, {}, {}, {}, {}]
 export const KanbanBoard = () => {
+    // Todo: How many Kanbanboard you want to create
+    //       kanbanBoard : [{title :  ,key: },{title : , key: },{title : , key: }] 
+    //       kanbanCard  : [{title :  , belongTo: KanbanBoardkey,  }]
+
+
+    // 
 
     return ( 
         <></>
