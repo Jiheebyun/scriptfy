406e9eb533e421b607480d597c3c811d8b109230
Jihee Byun <jh910805jh@gmail.com>
2025-05-12 23:53:16 +0900

    added a comment for features

--- src/library/components/react/src/components/dragDrop/subComponents/kanbanBoard/KanbanBoard.tsx
+++ src/library/components/react/src/components/dragDrop/subComponents/kanbanBoard/KanbanBoard.tsx
@@ -28,25 +28,37 @@ export const KanbanBoard = () => {
     // 
 
     return ( 
-        <div className="sf-kanban-board-layout">
-            <div className="sf-kanban-board-container">
-                <div className="kanban-board-line">
-
-                </div>
-                <div className="kanban-board-content">
-                    <div className="kanban-board-title">
+        <div className="sf-kanban-layout">
+            <div className="sf-kanban-container">
+                <div className="sf-kanban-board-container">
+                    <div className="kanban-board-line">
 
                     </div>
-                    <div className="kanban-board-options">
+                    <div className="kanban-board-content">
+                        <div className="kanban-board-title">
 
+                        </div>
+                        <div className="kanban-board-options">
+
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="sf-kanban-card-container">
+                <div className="sf-kanban-card-container">
 
+                </div>
             </div>
         </div>
+
     )
 }
 
+// Todo:
+// 칸반의 맨 뒤에있는 배경색을 바꿀수 있는가?
+// 칸반 보드의 색을 변경할수 있는가?
+// 여러개의 칸반 보드가 있을 경우, 화면이 줄어들때, 칸반 보드가 아래로 내려가 여려개의 행을 만들수 있는가? 
+// 각 칸반 보드 타이틀을 설정할수 있는가 
+// 각 칸반 보드에서 옵션으로 카드의 리스트의 개수를 출력해주는가 
+// 칸반 카드가 많아지면 스크롤을 할수 있는가
+// 칸반 카드가 없을경우, "No data available" 같은 리스트가 없다는 표시를 출력해 주는가? 
+// 카반 카드가 다른 칸반 보드로 드래드를 통해서 이동할수 있는가?
