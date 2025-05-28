8cd16fb1a180317888eb73d76cae249ec8cab8d7
Jihee Byun <jh910805jh@gmail.com>
2025-05-28 23:40:00 +0900

    added a comment

--- src/library/components/react/src/components/dragDrop/subComponents/kanbanBoard/KanbanBoard.tsx
+++ src/library/components/react/src/components/dragDrop/subComponents/kanbanBoard/KanbanBoard.tsx
@@ -37,6 +37,7 @@ export const KanbanBoard = ({
 
     // TODO - 보드 데이터와 카드 데이터를 나눠야함. 
     // TODO - 보드 css 작업
+    // TODO - 카드 컴포넌트를 분리
     const [boards, setBoards] = useState({
         progress: [
             { id: 'c1', title: 'Login UI' },
